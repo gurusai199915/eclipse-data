1148618c53e70bad30d86fddb52856ee0f3761a2
gurusai199915 <shankarkalamsetty@gmail.com>
2025-09-26 17:26:14 +0530

    create a new branch for negative cases

---
+++ src/facebooknegative/Fbloginwithspecialcharacters.java
@@ -0,0 +1,12 @@
+package facebooknegative;
+
+public class Fbloginwithspecialcharacters 
+{
+public static void main(String[] args)
+{
+System.out.println("user able to enter the data  user name as 1234##1234");
+System.out.println("user able to enter the  password as +-_+-");
+System.out.println("user able to click on login");
+
+}
+}
